6aefa437b2356bc6d78ebb0de968fcd2844a89a7
Scott Reynolds <sreynolds@twilio.com>
2014-04-12 13:24:45 -0700

    test the list function

--- tests/check_lval.c
+++ tests/check_lval.c
@@ -22,11 +22,42 @@ MU_TEST(test_lval_cons) {
     mu_assert(result->cell[0]->num == 1,
               "Cons should put the number one in front");
 
+    lval_delete(v);
+}
+
+MU_TEST(test_lval_list_success) {
+    lval* v = lval_sexpr();
+    lval* x = lval_num(1);
+    lval* y = lval_num(4);
+
+    v = lval_add(v, x);
+    v = lval_add(v, y);
+
+    lval* result = builtin(v, "list");
+    mu_assert(result->type == LVAL_QEXPR,
+              "List should turn an sexpr into a qexpr");
+    lval_delete(v);
+}
+
+MU_TEST(test_lval_list_qexpr) {
+    lval* v = lval_qexpr();
+    lval* x = lval_num(1);
+    lval* y = lval_num(4);
+
+    v = lval_add(v, x);
+    v = lval_add(v, y);
+
+    lval* result = builtin(v, "list");
+    mu_assert(result->type == LVAL_ERR,
+              "When list function is handed a non-sexpr it should error out.");
+    lval_delete(v);
 }
 
 MU_TEST_SUITE(builtin_suite)
 {
     MU_RUN_TEST(test_lval_cons);
+    MU_RUN_TEST(test_lval_list_success);
+    MU_RUN_TEST(test_lval_list_qexpr);
 }
 
 int main() {
