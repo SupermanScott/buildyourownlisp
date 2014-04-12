5b47b94c458cb5149d1665c3739c2e70bbab5106
Scott Reynolds <sreynolds@twilio.com>
2014-04-12 13:54:58 -0700

    tests for eval and join

--- tests/check_lval.c
+++ tests/check_lval.c
@@ -53,11 +53,94 @@ MU_TEST(test_lval_list_qexpr) {
     lval_delete(v);
 }
 
+MU_TEST(test_lval_eval_success) {
+    lval* v = lval_sexpr();
+    lval* q = lval_qexpr();
+    lval_add(q, lval_sym("+"));
+    lval_add(q, lval_num(1));
+    lval_add(q, lval_num(2));
+
+    lval_add(v, q);
+
+    lval* result = builtin(v, "eval");
+    mu_assert(result->type == LVAL_NUM,
+              "Eval a qexpr of {+ 1 2} should result in a number");
+    mu_assert(result->num == 3,
+              "Eval an qexpr of {+ 1 2} should result in 3");
+    lval_delete(result);
+}
+
+MU_TEST(test_lval_eval_too_large) {
+    lval* v = lval_sexpr();
+    lval_add(v, lval_sym("+"));
+    lval_add(v, lval_num(1));
+    lval_add(v, lval_num(2));
+
+    lval* result = builtin(v, "eval");
+    mu_assert(result->type == LVAL_ERR,
+              "When passed more then on Qexpr, eval should error out");
+    lval_delete(result);
+}
+
+MU_TEST(test_lval_eval_non_qexpr) {
+    lval* v = lval_sexpr();
+    lval_add(v, lval_sym("+"));
+
+    lval* result = builtin(v, "eval");
+    mu_assert(result->type == LVAL_ERR,
+              "When not passed a qexpr eval should error out");
+    lval_delete(result);
+}
+
+MU_TEST(test_lval_join_success) {
+    lval* v = lval_sexpr();
+    lval* q = lval_qexpr();
+    lval* y = lval_qexpr();
+
+    q = lval_add(q, lval_num(1));
+    q = lval_add(q, lval_num(4));
+
+    y = lval_add(y, lval_num(10));
+    y = lval_add(y, lval_num(14));
+
+    v = lval_add(v, q);
+    v = lval_add(v, y);
+
+    lval* result = builtin(v, "join");
+    mu_assert(result->type == LVAL_QEXPR,
+              "Joining two qexpr should result in a new qexpr");
+    mu_assert(result->count == 4,
+              "Joining two qexpr of size 2 should result in a qexpr of size 4");
+    lval_delete(result);
+}
+
+MU_TEST(test_lval_join_non_qexpr) {
+    lval* v = lval_sexpr();
+    lval* q = lval_qexpr();
+    lval* y = lval_num(60);
+
+    q = lval_add(q, lval_num(1));
+    q = lval_add(q, lval_num(4));
+
+    v = lval_add(v, q);
+    v = lval_add(v, y);
+
+    lval* result = builtin(v, "join");
+    mu_assert(result->type == LVAL_ERR,
+              "Joining a qexpr to a number should error out");
+    lval_delete(result);
+}
+
 MU_TEST_SUITE(builtin_suite)
 {
     MU_RUN_TEST(test_lval_cons);
     MU_RUN_TEST(test_lval_list_success);
     MU_RUN_TEST(test_lval_list_qexpr);
+    MU_RUN_TEST(test_lval_eval_success);
+    MU_RUN_TEST(test_lval_eval_too_large);
+    MU_RUN_TEST(test_lval_eval_non_qexpr);
+    MU_RUN_TEST(test_lval_join_success);
+    MU_RUN_TEST(test_lval_join_non_qexpr);
 }
 
 int main() {
